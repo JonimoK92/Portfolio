78325b318fdc89f82c51ddff3ce1bad5e042c3cb
JonimoK92 <jonathankmwela@gmail.com>
2026-06-08 14:01:32 +0200

    update 2

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 const observer = new IntersectionObserver(entries => {
     entries.forEach(entry => {
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.add("show");
         }
     });
 });
 
-document.querySelectorAll(".hidden").forEach(section=>{
+document.querySelectorAll(".hidden").forEach(section => {
     observer.observe(section);
+});
+
+const dropdown = document.querySelector(".cv-dropdown");
+
+dropdown.addEventListener("click", () => {
+    dropdown.classList.toggle("active");
+});
+
+// fermer si clic ailleurs
+document.addEventListener("click", (e) => {
+    if (!dropdown.contains(e.target)) {
+        dropdown.classList.remove("active");
+    }
 });
